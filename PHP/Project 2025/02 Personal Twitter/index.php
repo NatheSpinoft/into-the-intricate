549a70da7631d8721c91d50862d2c5e5ac17a2b0
Nathe Spinoft <stefanhpinto@gmail.com>
2025-11-22 16:50:17 -0500

    Styles

--- PHP/Project 2025/02 Personal Twitter/index.php
+++ PHP/Project 2025/02 Personal Twitter/index.php
@@ -4,19 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mini Chirper</title>
-    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="stylesheet" href="./assets/css/styles-login.css">
+    <link rel="icon" href="./assets/bird.png" type="image/png">
 </head>
 <body>
-    <h1>Welcome to Mini Chirper</h1>
     <div class="login-container">
+    <h1>Welcome to Mini Chirper</h1>
+    <div class="login-box">
         <form action="login.php" method="POST">
             <label for="username">Username:</label><br>
-            <input type="text" id="username" name="username" required><br>
+            <input type="text" id="username" name="username" placeholder="John" required><br>
             <label for="password">Password:</label><br>
             <input type="password" id="password" name="password" required><br>
             <input type="submit" value="Login">
         </form>
         <p>Don't have an account? <a href="register.php">Register here</a></p>
-    
+    </div>
+    </div>
 </body>
 </html>
